1451714e99d71c53080a01ff7e2c7f45752ff4f4
Yaohua Xiong <xiongyaohua@gmail.com>
2021-10-20 14:53:50 +0800

    定义network类接口

---
+++ network.py
@@ -0,0 +1,34 @@
+class Network:
+    def __init__(self):
+        None
+
+    # 1. 增删查改
+    def add_node(self):
+        None
+
+    def remove_node(self, nodeid):
+        None
+
+    def add_link(self, from_nodeid, to_nodeid):
+        None
+
+    def remove_link(self, from_nodeid, to_nodeid):
+        None
+
+    # 2. 查询
+    def from_nodes(self, nodeid):
+        None
+
+    def to_nodes(self, nodeid):
+        None
+
+    def is_connected(self, from_nodeid, to_nodeid):
+        None
+
+    # 3. 属性
+    def attributes(self, from_nodeid, to_nodeid):
+        None
+
+    # 4. 最短路径搜索
+    def shortest_routes_from(self, nodeid):
+        None
