53871243177a18f8d6bce0680d5b34fb796cc091
Yaohua Xiong <xiongyaohua@gmail.com>
2021-10-27 15:57:26 +0800

    实现最短路径算法

--- network.py
+++ network.py
@@ -1,3 +1,5 @@
+from heapq import heappush, heappop
+
 class Network:
     def __init__(self):
         self._neighbours = {}
@@ -52,19 +54,71 @@ class Network:
             del attribute[(from_nodeid, to_nodeid)]
 
     # 2. 查询
-    def from_nodes(self, nodeid):
+    def next_nodes(self, nodeid):
         return self._neighbours[nodeid]
 
-    def to_nodes(self, nodeid):
+    def prev_nodes(self, nodeid):
         None
 
     def is_connected(self, from_nodeid, to_nodeid):
         None
 
+    def nodes(self):
+        self._neighbours.keys()
+
     # 3. 属性
     def attributes(self, from_nodeid, to_nodeid):
         None
 
+    def cost(self, from_nodeid, to_nodeid):
+        self.attributes(from_nodeid, to_nodeid)["cost"]
+
     # 4. 最短路径搜索
     def shortest_routes_from(self, nodeid):
-        None
+        known = []
+        unknow = []
+        boundary = []
+        prevs = {}
+        costs = {}
+
+        # 初始化
+        for node in self.nodes():
+            unknow.append(node)
+            prevs[node] = None
+            costs[node] = 10000000
+
+        heappush(boundary, (0, nodeid))
+
+        # 搜索循环
+        while(boundary):
+            cost, current_node = heappop(boundary)
+            if current_node in known:
+                continue
+            known.append(current_node)
+            costs[current_node] = cost
+
+            for next_node in  self.next_nodes():
+                new_cost = cost + self.cost(current_node, next_node)
+                if new_cost < costs[next_node]:
+                    prevs[next_node] = current_node
+                    costs[next_node] = cost
+
+                    heappush(boundary, (new_cost, next_node))
+
+        # 重构路径
+        routes = {}
+        for node in self.nodes():
+            routes[node] = None
+
+        routes[nodeid] = [nodeid]
+
+        while None in routes.values():
+            for end_node, route in routes.items():
+                if route is not None:
+                    continue
+                prev = prevs[end_node]
+                if routes[prev] is not None:
+                    routes[end_node] = routes[prev] + [end_node]
+
+        return costs, routes
+
